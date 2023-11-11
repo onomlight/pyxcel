b87d1990eadf8ae1e4039d6be0c14c00ee886871
moon <102299695+onomlight@users.noreply.github.com>
2023-11-11 23:05:14 +0900

    Update fileload.py

--- fileload.py
+++ fileload.py
@@ -7,6 +7,8 @@ output_file_path = 'loadsave.xlsx'
 # 엑셀 파일 불러오기
 df = pd.read_excel(file_path)
 
+
+#A안
 # 'O' 열 초기화
 df['O'] = ''
 
@@ -29,6 +31,8 @@ with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
 print("작업이 완료되었습니다.")
 
 
+#B안
+
 # 이미 수정이 완료된건은 
 if 'Unnamed: 14' not in df.columns:
     raise ValueError("'Unnamed: 14' 열이 데이터프레임에 존재하지 않습니다.")
@@ -40,3 +44,5 @@ result_df = pd.DataFrame(output_list, columns=['(M, N): O'])
 
 # DataFrame을 엑셀 파일로 저장
 result_df.to_excel('output.xlsx', index=False)
+
+print("작업이 완료되었습니다.")
