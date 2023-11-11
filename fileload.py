6bb264ef56e50a463402a6b261a489a51c72159f
moon <102299695+onomlight@users.noreply.github.com>
2023-11-11 23:02:21 +0900

    MNO열 다 추가하기

--- fileload.py
+++ fileload.py
@@ -27,3 +27,16 @@ with pd.ExcelWriter(output_file_path, engine='xlsxwriter') as writer:
     mapping_df.to_excel(writer, sheet_name='Mapping', index=False)
 
 print("작업이 완료되었습니다.")
+
+
+# 이미 수정이 완료된건은 
+if 'Unnamed: 14' not in df.columns:
+    raise ValueError("'Unnamed: 14' 열이 데이터프레임에 존재하지 않습니다.")
+
+# 딕셔너리 형식으로 저장
+output_list = [f"('{m}', '{n}': '{o}')," for m, n, o in zip(df['상품명'], df['옵션명'], df['Unnamed: 14'])]
+# 리스트를 DataFrame으로 변환
+result_df = pd.DataFrame(output_list, columns=['(M, N): O'])
+
+# DataFrame을 엑셀 파일로 저장
+result_df.to_excel('output.xlsx', index=False)
